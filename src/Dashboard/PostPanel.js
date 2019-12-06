53fda55bfa0e12af2e92c312b3bb6ae8aeb549a8
Sainath Parakala <sainath256@gmail.com>
2019-12-05 22:58:29 -0500

    added search

--- src/Dashboard/PostPanel.js
+++ src/Dashboard/PostPanel.js
@@ -5,111 +5,142 @@ import ShareForm from './ShareForm';
 import EventPanel from './EventPanel';
 import ChatPanel from './ChatPanel';
 import Cookie from './../Utility/Cookie';
-class PostPanel extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {panel: "post", posts: [], tags: [], showPostForm: props.showPostForm, isPostFormHidden: props.isPostFormHidden, showShareForm: false};
-        this.getPosts = this.getPosts.bind(this);
-        this.showShareForm = this.showShareForm.bind(this);
-        this.changePanel = this.changePanel.bind(this);
-        this.getProfile();
-        this.postID = "";
+import SearchBox from '../SearchBox/SearchBox';
+class PostPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      panel: 'post',
+      posts: [],
+      tags: [],
+      showPostForm: props.showPostForm,
+      isPostFormHidden: props.isPostFormHidden,
+      showShareForm: false
+    };
+    this.getPosts = this.getPosts.bind(this);
+    this.showShareForm = this.showShareForm.bind(this);
+    this.changePanel = this.changePanel.bind(this);
+    this.getProfile();
+    this.postID = '';
+  }
+
+  showShareForm(postID) {
+    this.postID = postID;
+    this.setState({ showShareForm: !this.state.showShareForm });
+  }
+
+  renderShareForm() {
+    if (this.state.showShareForm) {
+      return (
+        <ShareForm postID={this.postID} showShareForm={this.showShareForm} />
+      );
+    } else {
+      return '';
     }
+  }
 
-    showShareForm(postID){
-        this.postID = postID;
-        this.setState({showShareForm: !this.state.showShareForm});
+  getPosts(tags) {
+    var options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + Cookie.getCookie('HC_JWT')
+      }
+    };
+    fetch(
+      'http://' +
+        process.env.REACT_APP_API_HOST +
+        '/posts/getPosts?tags=' +
+        JSON.stringify(tags),
+      options
+    )
+      .then(result => {
+        return result.json();
+      })
+      .then(result => {
+        this.setState({ posts: result.return });
+      });
+  }
+
+  getProfile() {
+    var options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + Cookie.getCookie('HC_JWT')
+      }
+    };
+
+    fetch(
+      'http://' + process.env.REACT_APP_API_HOST + '/profiles/profile',
+      options
+    )
+      .then(result => {
+        return result.json();
+      })
+      .then(result => {
+        this.getPosts(result.interests);
+      });
+  }
+  renderPostForm() {
+    if (this.state.isPostFormHidden()) {
+      return <PostForm closeForm={this.state.showPostForm} />;
+    } else {
+      return;
     }
+  }
 
-    renderShareForm(){
-        if(this.state.showShareForm){
-            return (
-                <ShareForm postID={this.postID} showShareForm={this.showShareForm}/>
-            );
-        }else{
-            return("");
-        }
-    }
+  changePanel(event) {
+    this.setState({ panel: event.target.textContent.toLowerCase() });
+  }
 
-    getPosts(tags){
-        var options = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + Cookie.getCookie('HC_JWT')
-            }
-        };
-        fetch("http://"+ process.env.REACT_APP_API_HOST +"/posts/getPosts?tags=" + JSON.stringify(tags), options).then( result => {
-            return result.json();
-        }).then(result => {
-            this.setState({posts: result.return});
-        });
-    }
-
-    getProfile(){
-        var options = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + Cookie.getCookie('HC_JWT')
-            }
-        };
-
-        fetch("http://"+ process.env.REACT_APP_API_HOST +"/profiles/profile", options).then( result => {
-            return result.json();
-        }).then( result => {
-            this.getPosts(result.interests);
-        });
-    }
-    renderPostForm(){
-
-        if(this.state.isPostFormHidden()){
-            return <PostForm closeForm={this.state.showPostForm} />
-        }else{
-            return;
-        }
-        
-    }
-
-    changePanel(event){
-        this.setState({panel: event.target.textContent.toLowerCase()});
-    }
-
-    render(){
-        if(this.state.panel === "post"){
-            return (
-            <div id="dash_postPanel">
-                <nav>
-                    <div className="p-fixed bg-primary border-lg w-100">
-                        <ul className="d-flex">
-                            <li className="cursor-pointer active">Home</li>
-                            <li className="cursor-pointer" onClick={this.changePanel}>Chats</li>
-                            <li className="cursor-pointer" onClick={this.changePanel}>Events</li>
-                        </ul>
-                    </div>
-                </nav>
-                {this.renderPostForm()}
-                {this.renderShareForm()}
-                <ul className="posts" >
-                    {
-                        this.state.posts.map((post, i) => 
-                        <Post id={post._id} title={post.title} key={i}
-                            tags={post.tags} dislikes={post.numDislikes}
-                            likes={post.numLikes} comments={post.comments}
-                            type={post.type} content={post.content}
-                            user={post.user} name={post.name}
-                            showShareForm={this.showShareForm}
-                            shareable={true} vote={post.vote}
-                        />)
-                    }
-                </ul>
+  render() {
+    if (this.state.panel === 'post') {
+      return (
+        <div id="dash_postPanel">
+          <nav>
+            <div className="p-fixed bg-primary border-lg w-100">
+              <ul className="d-flex">
+                <li className="cursor-pointer active">Home</li>
+                <li className="cursor-pointer" onClick={this.changePanel}>
+                  Chats
+                </li>
+                <li className="cursor-pointer" onClick={this.changePanel}>
+                  Events
+                </li>
+                <SearchBox />
+              </ul>
             </div>
-            );
-        }else if(this.state.panel === "chats"){
-            return (<ChatPanel />);
-        }else{
-            return (<EventPanel />);
-        }
+          </nav>
+          {this.renderPostForm()}
+          {this.renderShareForm()}
+          <ul className="posts">
+            {this.state.posts.map((post, i) => (
+              <Post
+                id={post._id}
+                title={post.title}
+                key={i}
+                tags={post.tags}
+                dislikes={post.numDislikes}
+                likes={post.numLikes}
+                comments={post.comments}
+                type={post.type}
+                content={post.content}
+                user={post.user}
+                name={post.name}
+                showShareForm={this.showShareForm}
+                shareable={true}
+                vote={post.vote}
+              />
+            ))}
+          </ul>
+        </div>
+      );
+    } else if (this.state.panel === 'chats') {
+      return <ChatPanel />;
+    } else {
+      return <EventPanel />;
     }
+  }
 }
 export default PostPanel;
---
+++ src/SearchBox/SearchBox.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import Downshift from 'downshift';
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+import searchBoxIcon from '../images/search.png';
+const inputStyles = {
+  width: '410px',
+  height: '40px',
+  borderRadius: '2px',
+  border: 'none',
+  backgroundColor: 'rgba(138,138,138,0.08)',
+  padding: '5px 8px',
+  fontSize: '15px',
+  fontWeight: '500',
+  marginTop: '11px'
+};
+
+const dropdownStyles = highLighted => {
+  return {
+    borderBottom: '1px solid wheat',
+    backgroundColor: highLighted ? '#f7f7f7' : 'white',
+    padding: '6px',
+    transition: 'all 0.2s',
+    paddingLeft: highLighted ? '3px' : '0',
+    borderLeft: `10px solid ${highLighted ? 'wheat' : 'white'}`,
+    maxWidth: '410px'
+  };
+};
+class SearchBox extends Component {
+  state = {
+    posts: [],
+    isLoading: false
+  };
+
+  onInputChange = debounce(async ({ target: { value } }) => {
+    if (value.length > 3) {
+      this.setState({ isLoading: true });
+      const {
+        data: { result }
+      } = await axios.get(`http://localhost:8080/searchposts?text=${value}`);
+      this.setState({ posts: result, isLoading: false });
+    }
+  }, 500);
+
+  render() {
+    return (
+      <Downshift
+        onChange={selection => alert(`you selected ${selection}`)}
+        itemToString={item => (item ? item.content : '')}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          getLabelProps,
+          getMenuProps,
+          isOpen,
+          inputValue,
+          highlightedIndex,
+          selectedItem
+        }) => (
+          <div>
+            <input
+              {...getInputProps({
+                onChange: e => {
+                  e.persist();
+                  this.onInputChange(e);
+                },
+                placeholder: 'Search posts'
+              })}
+              style={inputStyles}
+            />
+            <img
+              src={searchBoxIcon}
+              alt="searchbox Icon"
+              style={{
+                marginTop: '10px',
+                paddingTop: '4px',
+                position: 'relative',
+                top: '7px',
+                right: '39px'
+              }}
+            />
+            {isOpen &&
+              this.state.posts.map((post, index) => (
+                <p
+                  {...getItemProps({ key: index, index, item: post })}
+                  style={dropdownStyles(highlightedIndex === index)}
+                >
+                  {post.content}{' '}
+                </p>
+              ))}
+          </div>
+        )}
+      </Downshift>
+    );
+  }
+}
+
+export default SearchBox;
